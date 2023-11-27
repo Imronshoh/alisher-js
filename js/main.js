c4ee9e68598a847d3e73c02485abec37c1dd0dbb
Imronshoh <monikapi2022@gmail.com>
2023-11-27 17:06:52 +0500

    k

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var elDollar = document.querySelector(".h1");
+a = prompt("Alisher, nech pul topladis?");
+
+if (a > 9570000) {
+  elDollar.textContent = "Alisher, oq yol!";
+} else if (a == 9570000) {
+  elDollar.textContent = "Alisher, oq yol!";
+} else {
+  elDollar.textContent = "Alisher, davay pul toplovir!";
+}
